5671d51dc577e5da2e982628c75325f81565f331
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 21:30:16 +0530

    Create Farenheit_to_Celsius.c

---
+++ Lab/Farenheit_to_Celsius.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Converting Farenheit To Celsius//
+
+int main() {
+    float f = 45.5;
+    float c;
+
+    c = (5.0 / 9.0) * (f - 32);
+
+    printf("Celsius = %f", c);
+
+    return 0;
+}
