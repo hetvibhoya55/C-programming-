af6a8b7f230b2300490047dcdbc1574f75d2baf8
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 21:34:57 +0530

    Create Calculate_Interest.c

---
+++ Lab/Calculate_Interest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Calcute Interest//
+
+int main() {
+    float p = 45.5;
+    float r = 50.0;
+    float n = 55.0;
+    float sp;
+
+    sp =  (p*r*n)/100;
+
+    printf("Simple Interest = %f", sp);
+
+    return 0;
+}
