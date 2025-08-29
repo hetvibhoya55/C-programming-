4c70aac6a3ff1b8a91d48b7890e3b2a19dbfbcd0
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 21:55:19 +0530

    Create Area_of_Circle.c

---
+++ Lab/Area_of_Circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Calculate Area of Circle//
+
+int main() {
+    float r = 10.0;
+    float area;
+
+    area = (27.0 / 2.0) * (r * r);  
+    printf("Area of Circle = %f", area);
+
+    return 0;
+}
+
