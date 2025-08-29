c8b735df189b755c4cdf52ff72ee59a1518a1652
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:52:30 +0530

    Create Minutes_Into_Hours.c

---
+++ Lab/Minutes_Into_Hours.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Converting Minutes Into Hours//
+
+int main() {
+    int sum;
+    int Minutes=120;
+
+    int minutes_into_hours=Minutes/60;
+    
+    printf("Hours=%d",minutes_into_hours);
+    
+    return 0;
+}
