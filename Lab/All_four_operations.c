74f67b74194a9fda846f06ce79f1854cceb99627
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:44:13 +0530

    Create All_four_operations.c

---
+++ Lab/All_four_operations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//All four operations//
+
+int main() {
+    int sum;
+    int a=10;
+    int b=5;
+
+    int add=a+b;
+    int substract=a-b;
+    int multiply=a*b;
+    int divide=a/b;
+    printf("Addition of numbers=%d\n",add);
+    printf("Substraction of numbers=%d\n",substract);
+    printf("Multiplication of numbers=%d\n",multiply);
+    printf("Division of numbers=%d",divide);
+
+    return 0;
+}
