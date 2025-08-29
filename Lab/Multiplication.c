521f54d4f1f1f2100c6df04e80499d8b5f9518c2
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:40:43 +0530

    Create Multiplication.c

---
+++ Lab/Multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Multiplication of numbers//
+
+int main() {
+    int sum;
+    int a=10;
+    int b=5;
+
+    int multiply=a*b;
+    printf("Multiplication of numbers=%d",multiply);
+    return 0;
