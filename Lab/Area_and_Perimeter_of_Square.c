190a42cafe4f2bb0951f6adea56c5d6129074d66
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 21:43:43 +0530

    Create Area_and_Perimeter_of_Square.c

---
+++ Lab/Area_and_Perimeter_of_Square.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//Calculate Area and Perimeter of Square//
+
+int main() {
+    float l = 10.0;
+
+    float area;
+    float p;
+
+    area = l*l;
+    p = 4*l;
+
+    printf("Area = %f\n", area);
+    printf("Perimeter = %f\n", p);
+
+    return 0;
+}
