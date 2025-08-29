137032e483441552288e282ccae605c36e8cd3e0
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 21:59:37 +0530

    Create Area_of_Triangle.c

---
+++ Lab/Area_of_Triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Calculate Area of Triangle//
+
+int main() {
+    float h = 10.0;
+    float l = 20.0;
+    float area;
+
+    area = (h * l) / 2;
+
+    printf("Area of Triangle = %f", area);
+
+    return 0;
+}
