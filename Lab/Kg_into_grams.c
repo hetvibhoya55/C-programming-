b5145c6a582611f74063abb7b90e9df44aae0e19
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 20:38:58 +0530

    Create Kg_into_grams.c

---
+++ Lab/Kg_into_grams.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Converting KG Into Grams//
+
+int main() {
+    int sum;
+    float kg=505.53;
+
+    float grams=kg*1000;
+    
+    printf("Grams=%d",grams);
+    
+    return 0;
+}
