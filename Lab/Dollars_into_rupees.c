900516098d5bc97654d21487c9a8db42447b8d78
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:58:15 +0530

    Create Dollars_into_rupees.c

---
+++ Lab/Dollars_into_rupees.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Converting Dollars Into Rupees//
+
+int main() {
+    int sum;
+    int dollars=100;
+
+    int rupees=dollars*48;
+    
+    printf("Rupees=%d",rupees);
+    
+    return 0;
+}
