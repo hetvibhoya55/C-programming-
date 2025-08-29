4689d358cbb4976953b43a3e75890543d6fcb772
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:43:11 +0530

    Create Division.c

---
+++ Lab/Division.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Division of numbers//
+
+int main() {
+    int sum;
+    int a=10;
+    int b=5;
+
+    int divide=a-b;
+    printf("Division of numbers=%d",divide);
+    return 0;
