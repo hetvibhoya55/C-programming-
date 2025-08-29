8591369813062dfc97361bf104f35bc28d485fcb
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 22:40:43 +0530

    Create Swap_Two_Values.c

---
+++ Lab/Swap_Two_Values.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Swap Two Values//
+
+int main() {
+    int a = 2;
+    int b = 4;
+    int c = 8;
+    int d = 10;
+
+    a = b;
+    b = c;
+    c = d;
+
+    printf("A = %d\n", a);
+    printf("B = %d\n", b);
+
+    return 0;
+}
