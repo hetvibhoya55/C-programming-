ad489ee7b6ca2a1789518e35f5304fa83db5e403
hetvibhoya55 <hetvibhoya55@gmail.com>
2025-08-29 19:13:59 +0530

    Create Substraction.c

---
+++ Lab/Substraction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Substraction of numbers//
+
+int main() {
+    int sum;
+    int a=10;
+    int b=5;
+
+    int sum=a-b;
+    printf("Substraction of numbers=%d",sum);
+    return 0;
+}
